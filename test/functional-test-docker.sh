4bbedbf9ce22093eb1c1c5014200e40053e8b95d
Oppodelldog <odog@netcologne.de>
2019-02-09 14:15:06 +0100

    plain-ci functional tests

--- test/functional-test-docker.sh
+++ test/functional-test-docker.sh
@@ -6,12 +6,26 @@ function finish {
     docker rm -f ${containerDNS}
     docker rm -f ${containerPong}
     docker rm -f ${containerDNSTester}
+    rm -rf ".build-artifacts"
 }
 trap finish EXIT
 
 projectDir="$( cd "$( dirname "${BASH_SOURCE[0]}" )/../" && pwd )"
 containerProjectDir="/go/src/github.com/Oppodelldog/docker-dns"
 testImage="golang:1.11.5"
+
+echo "building dnsserver before starting tests, since loading dependencies can take a while."
+
+docker run \
+ --name "build_dnsserver_${RANDOM}" \
+ --rm \
+ -i \
+ -v ${projectDir}:${containerProjectDir}  \
+ -w ${containerProjectDir} \
+ ${testImage} \
+ bash -c "go get ./... && mkdir -p .build-artifacts && go build -o .build-artifacts/dnsserver dnsserver/cmd/main.go && chmod -R 0777 .build-artifacts"
+
+echo "now go starting the test"
 
 containerDNS=$(
 docker run \
@@ -20,12 +34,15 @@ docker run \
  -d \
  -v ${projectDir}:${containerProjectDir}  \
  -v /var/run/docker.sock:/var/run/docker.sock \
- -w ${containerProjectDir}/dnsserver \
+ -w ${containerProjectDir} \
  ${testImage} \
- bash -c "go get ./... && go run cmd/main.go"
+ bash -c "cd dnsserver && ../.build-artifacts/dnsserver"
 )
 
+echo "started dnsserver in container ${containerDNS}"
+
 dnsIp=$(docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' ${containerDNS})
+echo "dns ip is ${dnsIp}"
 
 containerPong=$(
 docker run \
@@ -36,7 +53,8 @@ docker run \
  -w ${containerProjectDir}  \
  ${testImage} \
  bash -c "go run test/pong/main.go"
- )
+)
+echo "started test target 'pong' in container ${containerPong}"
 
 containerDNSTester=$(
 docker run \
@@ -49,6 +67,7 @@ docker run \
  ${testImage} \
  bash -c "go run test/dnslookup/main.go"
 )
+echo "started functional tests in container ${containerDNSTester}"
 
 docker logs -f ${containerDNSTester}
 
