80cdfc61d3f52134e954daa43ead9f462fd37682
Oppodelldog <odog@netcologne.de>
2019-07-19 16:24:02 +0200

    compile functional test using go image, then execute

--- .build/ci.sh
+++ .build/ci.sh
@@ -2,6 +2,6 @@
 
 APP_DIR="$(cd -P "$(dirname "${filename}")/..";pwd)"
 
-docker run --rm --name="docker-dns-go" -v ${APP_DIR}:/app -w /app golang:latest pwd && ls -al && go build -o tests .test/main.go
+docker run --rm --name="docker-dns-go" -v ${APP_DIR}:/app -w /app golang:latest bash -c "pwd && ls -al && go build -o tests .test/main.go"
 
 ./tests
